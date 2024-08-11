e753093981ce3569606b99a1640ab18df6c92cc0
wolfuardian <wolfuardian@gmail.com>
2024-08-12 03:26:26 +0800

    Move project files to EW namespace

---
+++ Assets/EW/HelloWorld/Runtime/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace EW.HelloWorld
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+}
